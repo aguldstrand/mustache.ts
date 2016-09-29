b2eb7967edb492e957aaf7a9a3fbb5bbe9843805
aguldstrand <anders.guldstrand@digitaslbi.com>
2016-09-29 13:16:00 +0200

    broke helper param support

--- V2/src/compiler/tokenizer.ts
+++ V2/src/compiler/tokenizer.ts
@@ -3,8 +3,18 @@ export function* enumerateTokens(template: string) {
         item.value = item.value.trim()
         if (item.type === TokenType.Block) {
             if (item.value[0] === '#') {
-                item.value = item.value.substr(1)
+                var match = /([ ]*[#a-zA-Z0-9_\-./]+)*/.exec(item.value)
+
+                item.value = match[0].substr(1).trim()
                 item.type = TokenType.EnterBlock
+
+                const params: string[] = (<HelperToken>item).params = []
+                for (var i = 1; i < match.length; i++) {
+                    params.push(match[i].trim())
+                }
+
+                console.error(JSON.stringify(item))
+
             } else if (item.value[0] === '/') {
                 item.value = item.value.substr(1)
                 item.type = TokenType.ExitBlock
@@ -62,6 +72,10 @@ function* enumerateTokensInner(template: string) {
 export interface Token {
     value: string
     type: TokenType
+}
+
+export interface HelperToken extends Token {
+    params: string[]
 }
 
 export enum TokenType {
